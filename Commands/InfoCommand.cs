cbc20b17c8ed1a39ab231ddfe09fde0a9a3350ec
Palente <Palente@users.noreply.github.com>
2023-01-26 01:25:45 +0100

    Protorype of InfoCommand

---
+++ Commands/InfoCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Discord.Interactions;
+
+namespace DevCommuBot.Commands
+{
+    internal class InfoCommand : InteractionModuleBase<SocketInteractionContext>
+    {
+
+        [SlashCommand("info", "Get information about the discord and the bot")]
+        public async Task GetInfo()
+        {
+            //ToDo: Use  https://api.github.com/repos/devcommu/Bot/contributors
+        }
+    }
+}
